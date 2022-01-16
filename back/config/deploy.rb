c8aaa7c0c50c6a5e27b5e74dcab7b60f9d780dd1
jdpro <joadorn@gmail.com>
2022-01-16 12:34:22 +0100

    Share public directory

--- back/config/deploy.rb
+++ back/config/deploy.rb
@@ -33,7 +33,7 @@ Rake::Task['deploy:migrate'].clear_actions
 # append :linked_files, "config/database.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
