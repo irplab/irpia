04eeebb4f5df5129d4e5de82a3464887b99f28be
jdpro <joadorn@gmail.com>
2022-02-25 14:10:35 +0100

    Field title

--- front/src/features/notice/Notice.js
+++ front/src/features/notice/Notice.js
@@ -155,7 +155,7 @@ export function Notice() {
                 <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                     <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                            htmlFor="grid-state">
-                        Type de ressource
+                        Domaine d'enseignement
                     </label>
                     <div className="relative mb-2">
                         <select
