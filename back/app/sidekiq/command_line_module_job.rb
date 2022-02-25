350a992eb384a117ab79f7f312e78e9f57720812
jdpro <joadorn@gmail.com>
2022-02-25 14:31:46 +0100

    Log instructions

--- back/app/sidekiq/command_line_module_job.rb
+++ back/app/sidekiq/command_line_module_job.rb
@@ -7,14 +7,16 @@ class CommandLineModuleJob
     params = data.map {|k,v|  "--#{k} \"#{v}\""}
     command = "cd #{Rails.application.config.batch_modules_dir}#{config['dir']}; #{config['exec']} #{config['args']&.join(' ')} #{params&.join(' ')}"
     stdout, stderr, status = Open3.capture3("bash -c #{Shellwords.escape(command)}")
-    Rails.logger.error(stdout, stderr, status)
+    Rails.logger.info(command)
     thread = SuggestionsThread.find({ job_id: @jid }).first
     if stdout.present?
+      Rails.logger.info(stdout)
       thread.status = 'complete'
       thread.result = stdout
       thread.save
       thread.suggestion.save
     else
+      Rails.logger.error(stderr)
       thread.status = 'error'
       thread.result = stderr
       thread.save
