96888e36251a3850b0a1bd9fe63652f5519cac85
jdpro <joadorn@gmail.com>
2022-07-24 07:06:04 +0200

    Build classfication tags for domain and level

--- back/app/services/metadata_processor.rb
+++ back/app/services/metadata_processor.rb
@@ -1,5 +1,7 @@
 class MetadataProcessor
 
+  include TripleStoreAccess
+
   attr_accessor :metadata
 
   def initialize(metadata)
@@ -23,27 +25,6 @@ class MetadataProcessor
     @metadata[:domain] = @metadata[:domain].concat(result.map { |r| r[:suggest_dom_out].to_s.split('/').last }).uniq
   end
 
-  def sparql_client
-    require 'sparql/client'
-    SPARQL::Client.new(Rails.application.config.sparql[:uri])
-  end
 
-  def domain_entension_query
-    <<HEREDOC
-  PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-  PREFIX scolomfr: <https://www.reseau-canope.fr/scolomfr/ontologie#>
-  PREFIX isothes: <http://purl.org/iso25964/skos-thes#>
-  select distinct ?suggest_dom_in ?suggest_dom_in_label ?suggest_dom_out ?suggest_dom_out_label   
-  where { 
-    VALUES (?suggest_dom_in) 
-      { ( <http://data.education.fr/voc/scolomfr/concept/[value]>) }
-  ?suggest_dom_in skos:prefLabel ?suggest_dom_in_label. 
-  ?suggest_dom_in skos:narrower* ?suggest_dom_out.
-  ?suggest_dom_out isothes:status ?status_suggest_dom_out.
-  filter(regex(?status_suggest_dom_out, "Actuel", "i" ))
-  ?suggest_dom_out skos:prefLabel ?suggest_dom_out_label.       
-  } order by ?suggest_dom_out_label
-HEREDOC
-  end
 end
 
