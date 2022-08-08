de5782dff19c72f1a02dad6813f09173b410b5ac
jdpro <joadorn@gmail.com>
2022-08-08 10:41:17 +0200

    Bnf client details

--- back/app/services/api/bnf_client.rb
+++ back/app/services/api/bnf_client.rb
@@ -1,27 +1,27 @@
 class Api::BnfClient
 
-  ISNI_SRU_HOST = 'http://catalogue.bnf.fr'
-  ISNI_SRU_PATH = '/api/SRU?version=1.2&operation=searchRetrieve&query=(aut.type%20any%20%22org%22)%20and%20(aut.accesspoint%20all%20%22$QUERY%22)'
-  DEFAULT_ROWS = 100
+  BNF_SRU_HOST = 'http://catalogue.bnf.fr'
+  BNF_SRU_PATH = '/api/SRU?version=1.2&operation=searchRetrieve&query=(aut.type%20any%20%22org%22)%20and%20(aut.accesspoint%20all%20%22$QUERY*%22)&startRecord=1&maximumRecords=$ROWS'
+  DEFAULT_ROWS = 20
   SOURCE_IDENTIFIER = 'ISNI'
 
   def get_suggestions(query:, reference_type: 'person')
     results = []
     begin
       unless query.blank?
-        path = ISNI_SRU_PATH.gsub('$QUERY', URI.encode_www_form_component(query)).gsub('$ROWS', DEFAULT_ROWS.to_s)
+        path = BNF_SRU_PATH.gsub('$QUERY', URI.encode_www_form_component(query)).gsub('$ROWS', DEFAULT_ROWS.to_s)
         results = get_results(path)
       end
       results << { name: 'Aucun résultat ISNI', disabled: true, source: SOURCE_IDENTIFIER } if results.blank?
     rescue Faraday::ConnectionFailed => e
       Rails.logger.error e.message
-      results << { name: 'Service ISNI indisponible' + ' : ' + e.message, identifier: 0, source: SOURCE_IDENTIFIER } if results.blank?
+      results << { name: 'Service BNF indisponible' + ' : ' + e.message, identifier: 0, source: SOURCE_IDENTIFIER } if results.blank?
     rescue JSON::ParserError => e
       Rails.logger.error e.message
-      results << { name: 'Réponse ISNI illisible' + ' : ' + e.message, identifier: 0, source: SOURCE_IDENTIFIER } if results.blank?
+      results << { name: 'Réponse BNF illisible' + ' : ' + e.message, identifier: 0, source: SOURCE_IDENTIFIER } if results.blank?
     rescue StandardError => e
       Rails.logger.error e.message
-      results << { name: 'Erreur inconnue ISNI' + ' : ' + e.message, identifier: 0, source: SOURCE_IDENTIFIER } if results.blank?
+      results << { name: 'Erreur inconnue BNF' + ' : ' + e.message, identifier: 0, source: SOURCE_IDENTIFIER } if results.blank?
     end
     results.compact
   end
@@ -29,7 +29,7 @@ class Api::BnfClient
   private
 
   def get_results(path)
-    Rails.logger.debug "Bnf API called : #{ISNI_SRU_HOST}/#{path}"
+    Rails.logger.debug "BNF API called : #{BNF_SRU_HOST}/#{path}"
     response = isni_sru_connexion.get *path
     doc = Nokogiri.parse(response.body)
     doc.remove_namespaces!
@@ -39,15 +39,15 @@ class Api::BnfClient
     organisations.compact.map do |org|
       hash = {}
       hash[:source] = SOURCE_IDENTIFIER
-      hash[:name] = org.xpath("datafield[@tag = '110' or @tag = '410']/subfield[@code ='a']/text()").map(&:text).flatten.uniq.join(" / ")
+      hash[:name] = org.xpath("datafield[@tag = '110' or @tag = '210' or @tag = '410']/subfield[@code ='a']/text()").map(&:text).flatten.uniq.join(", ")
       hash[:identifier] = org.xpath("datafield[@tag = '010']/subfield[@code ='a']/text()").text()
-      hash[:type] = org.xpath("datafield[@tag = '300']/subfield[@code ='a']/text()").map(&:text).flatten.uniq.join(" / ")
+      hash[:type] = org.xpath("datafield[@tag = '300']/subfield[@code ='a']/text()").map(&:text).flatten.uniq.join(", ")
       hash
     end
   end
 
   def isni_sru_connexion
-    Faraday.new(:url => ISNI_SRU_HOST) { |conn|
+    Faraday.new(:url => BNF_SRU_HOST) { |conn|
       conn.adapter :net_http
     }
   end
